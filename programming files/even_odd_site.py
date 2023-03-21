4111c3b25180712b4b6e28663a999e548e7fbe2d
jsdrisco <112837177+jsdrisco@users.noreply.github.com>
2023-03-21 17:31:13 -0400

    Update even_odd_site.py

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -4,6 +4,7 @@ with open("numbers.html") as f:
     # Add even and odd numbers
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
+    i = 0
     for i in range(50):
         if i % 2 == 0:
             f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
