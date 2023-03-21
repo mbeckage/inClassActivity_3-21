8ac2c98993ca02f49d823089f30c707dcdf3dfeb
hyunaaah <67725652+hak2711@users.noreply.github.com>
2023-03-21 17:36:55 -0400

    Fix #13
    1. change 3900000 into 4000000
    2. change into sum_even += b in line 7

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -2,9 +2,9 @@
 #make a sum of fibonacci with even numbers
 a, b = 0, 1
 sum_even = 0
-while b < 3900000:
+while b < 4000000:
     if b % 2 == 0:
-        sum_even -= b
+        sum_even += b
     a, b = b, a+b
 
 print(sum_even)
