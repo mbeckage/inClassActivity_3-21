6b939abdeaed0fcdd537b2937dbbd13dabf872a1
jsdrisco <112837177+jsdrisco@users.noreply.github.com>
2023-03-21 16:54:08 -0400

    Added bugs

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -1,12 +1,12 @@
-#include <iostream>
+
 
 int main()
 {
     int a = 0, b = 1;
     int sum_even = 0;
-    while (b < 4000000) {
+    while (b < -4000000) {
         if (b % 2 == 0) {
-            sum_even += b;
+            sum_even *= b;
         }
         int temp = b;
         b = a + b;
@@ -14,7 +14,7 @@ int main()
     }
     std::cout << sum_even << std::endl;
 
-    return 0;
+
 }
 
 // answer should be 4613732
