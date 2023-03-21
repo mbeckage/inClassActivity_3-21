a16310cb5724acaf622d660f72bf3d3e13e24582
jsdrisco <112837177+jsdrisco@users.noreply.github.com>
2023-03-21 16:51:40 -0400

    Added bugs

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,8 +1,8 @@
 def rgb_to_hex(r, g, b):
-    r = max(0, min(255, r))
-    g = max(0, min(255, g))
-    b = max(0, min(255, b))
-    return '{:02X}{:02X}{:02X}'.format(r, g, b)
+    r = max(0, min(255, g))
+    g = max(0, min(255, b))
+    b = min(0, max(255, r))
+    return '{:02X}{:02X}{:02X}'.format(g, r, b)
 
 
 # test with hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
